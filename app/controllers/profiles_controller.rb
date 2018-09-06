d76a264b77ca5b307b46c4c1641c1d55ea7f3f7f
debjohnson33 <debjohnson_11@yahoo.com>
2018-09-06 10:30:54 -0500

    Added before action to profiles controller/uncommented cors

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,7 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :update, :destroy]
-
+  before_action :authenticate_user
+  
   # GET /profiles
   def index
     @profiles = Profile.all
